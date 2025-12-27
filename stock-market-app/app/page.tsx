5c4d64f7953877c3fddae015e0333c9d1ecb4be7
Tommaso Paparesta <t.paparesta.3231@vallauri.edu>
2025-12-27 14:22:16 +0000

    Initial commit

---
+++ stock-market-app/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return(
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Click me</Button>
+    </div>
+  )
+};
+
+export default HomePage;
